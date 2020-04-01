d53375176a6578ca9332e151fea3c7cce7598291
Smith Carvajal <36459473+hamiltonsalazar@users.noreply.github.com>
2020-04-01 18:27:40 -0500

    Proyecto final
    Finalización del curso.

---
+++ boletines/javascript/main.js
@@ -0,0 +1,58 @@
+// DOM
+// querySelector=>1  querySelectorAll
+
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+  console.log(link);
+})
+*/
+
+/*
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+  td.addEventListener('click',function(){
+    console.log(this);
+  })
+});
+*/
+
+// Obtener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+// Recorrerlos.
+
+links.forEach(function(link){
+// Agregar un evento click a cada uno de ellos.
+  link.addEventListener('click',function(ev){
+    ev.preventDefault();
+
+    let content = document.querySelector(".content");
+
+    // Quitarle las clases de animación que ya Tiene
+
+    content.classList.remove("fadeInDown");
+    content.classList.remove("animated");
+
+    // Agregar clases par aanimar su salida
+    content.classList.add("fadeOutUp");
+    content.classList.add("animated");
+
+    setTimeout(function(){
+      location.href = "/";
+    }, 600);
+
+    //return false;
+  });
+});
+
+/*
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-star-half-alt");
+});
+*/
